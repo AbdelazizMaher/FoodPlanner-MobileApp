09fa03b25a1f92256baa85b4d8d856490d85a86c
ABDELAZIZ MAHER <abdelazizmaher17499@gmail.com>
2025-02-04 22:30:48 +0200

    implemnt splash and add it to navgraph

--- FoodPlanner/app/src/main/java/com/example/foodplanner/SplashScreenFragment.java
+++ FoodPlanner/app/src/main/java/com/example/foodplanner/SplashScreenFragment.java
@@ -1,5 +1,7 @@
 package com.example.foodplanner;
 
+import android.animation.Animator;
+import android.graphics.Typeface;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -9,9 +11,18 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.airbnb.lottie.LottieAnimationView;
+import com.bumptech.glide.Glide;
+
+import jp.wasabeef.glide.transformations.BlurTransformation;
 
 
 public class SplashScreenFragment extends Fragment {
+
+    private LottieAnimationView lottieAnimationView;
 
     public SplashScreenFragment() {
         // Required empty public constructor
@@ -32,5 +43,32 @@ public class SplashScreenFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+
+//        ImageView imageView = view.findViewById(R.id.backgroundImage);
+//        Glide.with(this)
+//                .load(R.drawable.background_image)
+//                .transform(new BlurTransformation(25, 3))
+//                .into(imageView);
+
+        lottieAnimationView = view.findViewById(R.id.lottieAnimationView);
+
+        lottieAnimationView.setAnimation(R.raw.a);
+        lottieAnimationView.playAnimation();
+
+        lottieAnimationView.addAnimatorListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                lottieAnimationView.setAnimation(R.raw.c);
+                lottieAnimationView.playAnimation();
+            }
+
+            @Override
+            public void onAnimationStart(Animator animation) {}
+            @Override
+            public void onAnimationCancel(Animator animation) {}
+            @Override
+            public void onAnimationRepeat(Animator animation) {}
+        });
     }
 }
