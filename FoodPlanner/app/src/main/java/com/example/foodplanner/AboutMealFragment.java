45d4c9cb1ad16eeae4b14906a86ee712c877d124
ABDELAZIZ MAHER <abdelazizmaher17499@gmail.com>
2025-02-10 00:20:30 +0200

    add meal-detail-ui page

---
+++ FoodPlanner/app/src/main/java/com/example/foodplanner/AboutMealFragment.java
@@ -0,0 +1,29 @@
+package com.example.foodplanner;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class AboutMealFragment extends Fragment {
+
+    public AboutMealFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_about_meal, container, false);
+    }
+}
