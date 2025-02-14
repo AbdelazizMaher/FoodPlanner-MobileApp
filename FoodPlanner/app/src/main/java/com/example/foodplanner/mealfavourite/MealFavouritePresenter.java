9859826c998f9a1dc72d41c95089a2994708cceb
ABDELAZIZ MAHER <abdelazizmaher17499@gmail.com>
2025-02-14 14:52:43 +0200

    add and remove favourite meal using room

---
+++ FoodPlanner/app/src/main/java/com/example/foodplanner/mealfavourite/MealFavouritePresenter.java
@@ -0,0 +1,36 @@
+package com.example.foodplanner.mealfavourite;
+
+import com.example.foodplanner.model.MealDTO;
+import com.example.foodplanner.model.MealRepository;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
+public class MealFavouritePresenter implements MealFavouriteContract.IPresenter {
+    private MealFavouriteContract.IView view;
+    private MealRepository repo;
+
+    public MealFavouritePresenter(MealFavouriteContract.IView view, MealRepository repo) {
+        this.view = view;
+        this.repo = repo;
+    }
+
+
+    @Override
+    public void fetchFavouriteMeals(String idUser) {
+        repo.getFavouriteMeals(idUser)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(meals -> {
+                    view.showFavouriteMeals(meals);
+                });
+    }
+
+    @Override
+    public void removeMealFromFavourite(MealDTO meal) {
+        repo.deleteMeal(meal)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe();
+    }
+}
